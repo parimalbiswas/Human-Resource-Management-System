3406a43d4c600cc0a11b64eb72f326810dbb7603
PARIMAL BISWAS <biswasparimal01@gmail.com>
2022-11-09 23:32:31 +0530

    .

--- HRMS Software/src/com/usecase/AdminVerificationDone.java
+++ HRMS Software/src/com/usecase/AdminVerificationDone.java
@@ -2,6 +2,8 @@ package com.usecase;
 
 import java.util.Scanner;
 
+import com.welcomScreen.WelcomeScreen;
+
 public class AdminVerificationDone
 {
 
@@ -22,6 +24,47 @@ public class AdminVerificationDone
 		System.out.println("7. Press 7 to exit ");
 		System.out.println("------------------------------------");
 		int choise = scanner.nextInt();
+
+		switch (choise) {
+		case 1:
+			System.out.println("You Pressed 1....");
+			RegisterNewEmployee_usecase_ADMIN.RegisterNewEmployee_usecaseX();
+			break;
+
+		case 2:
+			System.out.println("You Pressed 2....");
+			// UpdateProfile_usecase_EMP.updateProfile_usecaseX(employee);
+			break;
+
+		case 3:
+			System.out.println("You Pressed 3....");
+			// updatePassword_usecase_EMP.updatePassword_usecaseX(employee);
+			break;
+
+		case 4:
+			System.out.println("You Pressed 4....");
+			// RequestLeave_usecase_EMP.RequestLeave_usecaseX(employee);
+			break;
+
+		case 5:
+			System.out.println("You Pressed 5....");
+			// RequestLeave_usecase_EMP.RequestLeave_usecaseX(employee);
+			break;
+
+		case 6:
+			System.out.println("You Pressed 6....");
+			// RequestLeave_usecase_EMP.RequestLeave_usecaseX(employee);
+			break;
+
+		case 7:
+			WelcomeScreen.control();
+			break;
+
+		default:
+			System.out.println("Worng choise....");
+			verifiedAdminAcivity();
+			break;
+		}
 
 	}
 }
---
+++ HRMS Software/src/com/usecase/RegisterNewEmployee_usecase_ADMIN.java
@@ -0,0 +1,15 @@
+package com.usecase;
+
+import java.util.Scanner;
+
+public class RegisterNewEmployee_usecase_ADMIN
+{
+
+	public static void RegisterNewEmployee_usecaseX()
+	{
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("-----------------------------");
+
+	}
+}
